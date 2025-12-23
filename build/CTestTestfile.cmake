b44fa5e0636cdb2171753c1a7e7ae540222ccfa9
DESKTOP-P05H4J3\ASUS <kaitokit139@gmail.com>
2025-12-23 14:11:32 +0700

    Add build artifacts

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: D:/Hai/study/TTNT/GoGame
+# Build directory: D:/Hai/study/TTNT/GoGame/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_board]=] "D:/Hai/study/TTNT/GoGame/build/test_board.exe")
+set_tests_properties([=[test_board]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hai/study/TTNT/GoGame/CMakeLists.txt;153;add_test;D:/Hai/study/TTNT/GoGame/CMakeLists.txt;0;")
+add_test([=[test_minimax]=] "D:/Hai/study/TTNT/GoGame/build/test_minimax.exe")
+set_tests_properties([=[test_minimax]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hai/study/TTNT/GoGame/CMakeLists.txt;160;add_test;D:/Hai/study/TTNT/GoGame/CMakeLists.txt;0;")
+add_test([=[test_mcts]=] "D:/Hai/study/TTNT/GoGame/build/test_mcts.exe")
+set_tests_properties([=[test_mcts]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hai/study/TTNT/GoGame/CMakeLists.txt;167;add_test;D:/Hai/study/TTNT/GoGame/CMakeLists.txt;0;")
